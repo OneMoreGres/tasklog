211ea87f36e88adb08a2793346261b428f75cb3b
Gres <gres@gres.biz>
2019-11-10 14:58:35 +0300

    Added task debug operator

--- src/task.h
+++ src/task.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <QDateTime>
+#include <QDebug>
 
 struct Task {
   QString text;
   QDateTime date;
 };
+
+inline QDebug operator<<(QDebug s, const Task& v)
+{
+  s << "Task(" << v.date.toString(Qt::ISODate) << v.text << ")";
+  return s;
+}
