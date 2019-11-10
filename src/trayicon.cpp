445833a3572e7203cfd9ef419f608d98d2ba5d71
Gres <gres@gres.biz>
2019-11-10 14:14:17 +0300

    Added tray menu

--- src/trayicon.cpp
+++ src/trayicon.cpp
@@ -1,10 +1,32 @@
 #include "trayicon.h"
 
+#include <QMenu>
 #include <QSystemTrayIcon>
 
 TrayIcon::TrayIcon()
   : tray_(new QSystemTrayIcon(this))
 {
+  auto menu = new QMenu;
+  {
+    auto action = menu->addAction(tr("Add record"));
+    connect(action, &QAction::triggered,  //
+            this, &TrayIcon::addTaskRequested);
+  }
+  {
+    auto action = menu->addAction(tr("Show existing"));
+    connect(action, &QAction::triggered,  //
+            this, &TrayIcon::mainWindowRequested);
+  }
+  menu->addSeparator();
+  {
+    auto action = menu->addAction(tr("Quit"));
+    connect(action, &QAction::triggered,  //
+            this, &TrayIcon::quitRequested);
+  }
+
+  tray_->setContextMenu(menu);
+  tray_->setIcon(QIcon::fromTheme("edit-find"));
+  tray_->show();
 }
 
 TrayIcon::~TrayIcon() = default;
